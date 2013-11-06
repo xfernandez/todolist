9387651c67593ae984ece3dff878001ff2c0e578
Xavier Fernandez <xavi.fernandez@gmail.com>
2013-11-06 05:31:55 +0100

    changes

--- project/Build.scala
+++ project/Build.scala
@@ -5,12 +5,6 @@ object ApplicationBuild extends Build {
 
     val appName = "todolist"
     val appVersion = "1.0-SNAPSHOT"
-
-    val appDependencies = Seq(
-    )
-
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-    )
     
     libraryDependencies ++= Seq(
   	javaJdbc,
