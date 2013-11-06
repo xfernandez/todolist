af2809276c6bb7e1f89ff2ed76e72d450156fc3f
Xavier Fernandez <xavi.fernandez@gmail.com>
2013-11-06 05:37:44 +0100

    Infinite playing with heroku

--- project/Build.scala
+++ project/Build.scala
@@ -7,9 +7,6 @@ object ApplicationBuild extends Build {
     val appVersion = "1.0-SNAPSHOT"
     
     libraryDependencies ++= Seq(
-  	javaJdbc,
-  	javaEbean,
-  	cache,
   	"postgresql" % "postgresql" % "8.4-702.jdbc4"
     )
 
